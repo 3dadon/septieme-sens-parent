2681b962541c6b3a08d20d2b9f160214af425c26
phl <paul.henri.larbat@gmail.com>
2026-05-17 21:49:52 +0200

    [fix] : deleted wrong logo import and replaced it by a good one

--- septieme-sens-front/src/App.tsx
+++ septieme-sens-front/src/App.tsx
@@ -11,7 +11,6 @@ import {
   Utensils,
   Waves,
 } from "lucide-react";
-import logoUrl from "../dist/assets/7eme_sens_logo.png";
 
 const navItems = [
   { label: "Accueil", href: "#accueil", icon: Home },
@@ -109,7 +108,7 @@ function LogoMark() {
   return (
     <div className="relative mx-auto h-24 w-24 overflow-hidden rounded-full border border-gold/25 bg-ivory/70 p-3 shadow-[0_18px_70px_rgba(129,91,43,0.16)] lg:mx-0">
       <img
-        src={logoUrl}
+        src="/logo/7eme_sens_logo.png"
         alt="Logo Septième Sens"
         className="h-full w-full scale-[1.85] object-contain object-[50%_38%]"
       />
